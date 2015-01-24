2df132e568976a8f8d710b8f0cfd81af21b49f7e
Corey Ganong <coreyganong@gmail.com>
2015-01-24 17:44:57 -0400

    Not done users yet (going to the Diplomat)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,2 @@
-class User
-  attr_accessor :name, :email
-
-  def initialize(attributes = {})
-    @name = attributes[:name]
-    @email = attributes[:email]
-  end
-
-
+class User < ActiveRecord::Base
 end
