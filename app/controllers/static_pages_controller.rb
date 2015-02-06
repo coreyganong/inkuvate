d898b04b94a1428c0f806323426403e7984d4b21
Corey Ganong <coreyganong@gmail.com>
2015-02-06 00:03:35 -0400

    Temp for heroku

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,9 @@ class StaticPagesController < ApplicationController
   def help
   end
 
+  def temp
+    render :layout => false
+  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  root              'static_pages#home'
-  get 'contact' =>  'static_pages#contact'
-  get 'help'    =>  'static_pages#help'
-  get 'about'   =>  'static_pages#about'
-  get 'signup'  =>  'users#new'
-  get 'login'   =>  'sessions#new'
-  post 'login'   =>  'sessions#create'
-  delete 'logout'   =>  'sessions#destroy'
-  resources :users
+  root                'static_pages#temp'
+  # get 'sessions/new'
+  #
+  # root              'static_pages#home'
+  # get 'contact' =>  'static_pages#contact'
+  # get 'help'    =>  'static_pages#help'
+  # get 'about'   =>  'static_pages#about'
+  # get 'signup'  =>  'users#new'
+  # get 'login'   =>  'sessions#new'
+  # post 'login'   =>  'sessions#create'
+  # delete 'logout'   =>  'sessions#destroy'
+  # resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
